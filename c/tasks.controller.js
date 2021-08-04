4c077a292c456cd25e7fda848a06acfb41049aba
Deyan <1704796@uad.ac.uk>
2021-08-04 21:58:55 +0100

    changed relative file path for importing the model data

--- c/tasks.controller.js
+++ c/tasks.controller.js
@@ -1,12 +1,12 @@
 // importing the hardcoded list of tasks
-const tasks = require("./m/tasks.model.js").tasks;
+const tasks = require("../m/tasks.model.js").tasks;
 
-function getAll (req, res) {
-    return res.json(tasks);
+function getAll(req, res) {
+  return res.json(tasks);
 }
 
 // returning a specific task by ID
-function getById (req, res) {
+function getById(req, res) {
   let result = tasks.filter((task) => task.id === Number(req.params.id));
 
   return result.length > 0
@@ -15,7 +15,7 @@ function getById (req, res) {
 }
 
 // returning all tasks that match a search term
-function getByQuery (req, res) {
+function getByQuery(req, res) {
   let query = req.params.query.toLowerCase();
 
   // creating a new array with any search matches
