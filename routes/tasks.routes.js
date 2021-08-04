8b91d72924f55ba2db435188af1eab657649baa6
Deyan <1704796@uad.ac.uk>
2021-08-04 20:21:17 +0100

    Create tasks.routes.js

---
+++ routes/tasks.routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('./c/tasks.controller.js');
+
+router.get("/", function (req, res) {
+    res.send("<h1>Tasks REST API</h1> <p>The following paths are supported</p>");
+});
+
+router.get("/tasks", controller.getAll);
+router.get("/tasks/:id", controller.getById);
+router.get("/tasks/search/:query", controller.getByQuery);
