37205ae893a513b7e1ad6801331658978f327188
Deyan <1704796@uad.ac.uk>
2021-08-04 21:57:52 +0100

    fixed routing issue

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 // init project
-let express = require("express");
-let bodyParser = require("body-parser");
-const tasksRoute = require('./routes/tasks.routes.js').router;
-let app = express();
+const express = require("express");
+const bodyParser = require("body-parser");
+const tasksRoute = require("./routes/tasks.routes.js").router;
+const path = require("path");
+const app = express();
 
 // parses the JSON request body string into an object and then assigns it to req.body
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/v/index.html"));
+});
+
 // sending all API requests to routes file
-app.use('/tasks', tasksRoute);
-app.use('/tasks/*', tasksRoute);
+app.use(tasksRoute);
 
 // Listening on port 8080
-let listener = app.listen(8080, function () {
-    console.log("Listening on port " + listener.address().port);
+const listener = app.listen(8080, function () {
+  console.log("Listening on port " + listener.address().port);
 });
