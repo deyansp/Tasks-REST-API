2714a9fd189f338100d8cfa333924094f4017284
Deyan <1704796@uad.ac.uk>
2021-08-04 22:00:06 +0100

    fixed relative import path for controller, removed redundant '/' route

--- routes/tasks.routes.js
+++ routes/tasks.routes.js
@@ -1,10 +1,6 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const controller = require('./c/tasks.controller.js');
-
-router.get("/", function (req, res) {
-    res.send("<h1>Tasks REST API</h1> <p>The following paths are supported</p>");
-});
+const controller = require("../c/tasks.controller.js");
 
 router.get("/tasks", controller.getAll);
 router.get("/tasks/:id", controller.getById);
